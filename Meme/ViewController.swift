62f7e6ebbf8f7d3817a157cbe458607098259c91
Manpreet <manpreetsgarha@gmail.com>
2018-10-24 07:05:52 +0530

    imagepicker and textfields added

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -1,25 +1,71 @@
 //
 //  ViewController.swift
-//  Meme
+//  meme
 //
-//  Created by Manpreet Singh on 24/10/18.
+//  Created by Manpreet Singh on 22/10/18.
 //  Copyright © 2018 Manpreet Singh. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled=UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+        initTextFields()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    enum BarItems: Int {
+        case gallary
+        case camera
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image=image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openImageController(_ sender: UIBarItem) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        switch(BarItems(rawValue: sender.tag)!){
+        case .camera:
+            controller.sourceType = .camera
+        case .gallary:
+            controller.sourceType = .photoLibrary
+        }
+        
+        present(controller, animated: true, completion: nil)
+    }
+}
 
-
+extension ViewController: UITextFieldDelegate{
+    
+    func initTextFields(){
+        topText.delegate=self
+        bottomText.delegate=self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if(textField.text == "TOP" || textField.text == "BOTTOM"){
+            textField.text=""
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
